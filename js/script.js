76ede7decd04a5f57abbb38a548bf884db78e4db
constanza evaristo <connievaristo@gmail.com>
2024-05-20 10:50:11 -0300

    entrega updated

--- js/script.js
+++ js/script.js
@@ -1,43 +1,55 @@
-let monto = parseFloat(prompt("Indicá el monto que querés pagar en cuotas."));
-let cuotas = parseInt(prompt("Indicá en cuántas cuotas querés pagar (3, 6, 9 o 12)"));
-
-console.log("El usuario quiere pagar " + monto + " en " + cuotas +" cuotas");
-
+let monto;
+let cuotas;
 let montoCuotas;
 let montoInteres;
 
-const aplicarInteres = (monto,interes) => {return montoInteres = monto * interes};
+const aplicarInteres = (monto, interes) => { return montoInteres = monto * interes };
 
-function calculaMontoCuota(a,b) {
-    if(b > 0){
+function calculaMontoCuota(a, b) {
+    if (b > 0) {
         return montoCuotas = a / parseInt(b);
-    }else{
+    } else {
         "Ingrese un número válido. No se puede dividir por cero."
     };
 };
 
-switch (cuotas){
-    case 3:
-        aplicarInteres(monto,1.3);
-        calculaMontoCuota(monto,3);
-        break;
-    case 6:
-        aplicarInteres(monto,1.6);
-        calculaMontoCuota(montoInteres,6);
-        break;
-    case 9:
-        aplicarInteres(monto,1.9);
-        calculaMontoCuota(montoInteres,9);
-        break;
-    case 12:
-        aplicarInteres(monto,1.12);
-        calculaMontoCuota(montoInteres,12);
-        break;
-    default:
-        console.log("No ingresó una cantidad de cuotas válida.");
-};
+do {
+    monto = parseFloat(prompt("Indicá el monto que querés pagar en cuotas."));
+    if (isNaN(monto || monto <= 0)){
+        alert("No ingresó un monto válido")
+    };
+} while (isNaN(monto) || monto <= 0);
 
-alert("Son " + cuotas + " cuotas de " + Math.round(montoCuotas) + ". El monto total es de "+ Math.round(montoInteres));
+do {
+    cuotas = parseInt(prompt("Indicá en cuántas cuotas querés pagar (3, 6, 9 o 12)"));
+    if (cuotas == 3 || cuotas == 6 || cuotas == 9 || cuotas == 12) {
+        switch (cuotas) {
+            case 3:
+                aplicarInteres(monto, 1.3);
+                calculaMontoCuota(monto, 3);
+                break;
+            case 6:
+                aplicarInteres(monto, 1.6);
+                calculaMontoCuota(montoInteres, 6);
+                break;
+            case 9:
+                aplicarInteres(monto, 1.9);
+                calculaMontoCuota(montoInteres, 9);
+                break;
+            case 12:
+                aplicarInteres(monto, 1.12);
+                calculaMontoCuota(montoInteres, 12);
+                break;
+            default:
+                console.log("Hubo un error en la operación");
+        };
+        console.log("El usuario quiere pagar " + monto + " en " + cuotas + " cuotas");
+    } else {
+        alert("No ingresó una cantidad de cuotas válida.");
+    }
+} while (cuotas !== 3 && cuotas !== 6 && cuotas !== 9 && cuotas !== 12);//Avanza si el usuario ingresa una de esas opciones, si no no sigue al resto del código.
+
+alert("Son " + cuotas + " cuotas de " + Math.round(montoCuotas) + ". El monto total es de " + Math.round(montoInteres));
 
 
 let nombreUsuario = prompt("Ahora vamos a pedirte algunos datos para contactarte. Por favor, ingresá tu nombre.");
@@ -51,4 +63,4 @@ if (edadUsuario < 18 && edadUsuario >= 0) {
     console.log(nombreUsuario + " tiene " + edadUsuario + " años. Su celular es " + celular);
 } else {
     alert("Por favor, ingresá un número válido.")
-};
+};    
