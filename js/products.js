0669370056b88cb9ce9c8f71fb76970e98eeeb3a
constanza evaristo <connievaristo@gmail.com>
2024-06-19 10:43:22 -0300

    products page ok

--- js/products.js
+++ js/products.js
@@ -1,48 +1,48 @@
 //Agregar productos al HTML
 const productos = [{
     id: 1,
-    product: "Mesa de luz",
-    precio: 50000
+    product: "Mesa de Luz",
+    precio: 150000
 },
 {
     id: 2,
     product: "Mesa comedor",
-    precio: 180000
+    precio: 200000
 },
 {
     id: 3,
     product: "Espejo",
-    precio: 35000
+    precio: 50000
 },
 {
     id: 4,
     product: "Rack de TV",
-    precio: 89500
+    precio: 250000
 },
 {
     id: 5,
     product: "Silla Windsor",
-    precio: 40000
+    precio: 45000
 },
 {
     id: 6,
     product: "Escritorio",
-    precio: 102000
+    precio: 110000
 },
 {
     id: 7,
     product: "Mesa de arrime",
-    precio: 120000
+    precio: 170000
 },
 {
     id: 8,
-    product: "Lámpara Pantalla de Bamboo",
-    precio: 70000
+    product: "Mesa Cava",
+    precio: 120000
 },
 {
     id: 9,
-    product: "Silla Tulum",
-    precio: 37000
+    product: "Sillón",
+    precio: 400000
 },
 {
     id: 10,
@@ -51,26 +51,101 @@ const productos = [{
 }
 ];
 
+let cart = cargarCartdeLocalStorage(); //Declaro abajo
 
-const divProd = document.createElement('div'); //Creo al div para los productos
-const sectionProd = document.getElementById('sectionProd'); //Traigo a la section que declaré en HTML con el id
-sectionProd.appendChild(divProd); //Anido al div de productos en la section
+//Función para agregar producto al carrito
+function addToCart(productID, cantidad) {
+    const producto = productos.find(p => p.id === productID); //Guardo al producto ahí cuando lo encuentro
+    if (!producto) { //Si no encuentro producto
+        console.error("Product not found");
+        return;
+    };
 
-for (const producto of productos) {
-    let literal = `Nombre: ${producto.product} | Precio $${producto.precio}`
-    const texto = document.createElement('p');
-    texto.innerHTML = literal; //Guardo texto en la etiqueta para que lo muestre
-    divProd.appendChild(texto); //Lo guardo en el div
+    const cartItem = cart.find(item => item.id === productID);
+
+    if (cartItem) {
+        cartItem.cantidad += cantidad;
+        cartItem.precioTotal = cartItem.cantidad * producto.precio;
+    } else {
+        cart.push({
+            id: producto.id,
+            product: producto.product,
+            precio: producto.precio,
+            cantidad: cantidad,
+            precioTotal: cantidad * producto.precio
+        });
+    }
+
+    guardarCartEnLocalStorage();
+    renderCart();
 };
 
-const botonID = document.getElementById('botonID');
-botonID.addEventListener('click',buscarId);
-
-function buscarId() {
-    const idIngresado = prompt("Ingresá el nombre del producto para ver el ID (tal cual está escrito en la descripción)");
-    console.log(idIngresado);
-
-    const resultadoFind = productos.find((producto) => producto.product === `${idIngresado}`);
-    alert(`El ID de ${resultadoFind.product} es ${resultadoFind.id}`);
-    console.log(resultadoFind)
+//Función para crear al elemento del producto para que se vea
+function renderProductos() {
+    const listaProducto = document.getElementById('sectionProd');
+    listaProducto.innerHTML = ''; //limpio
+    productos.forEach(producto => {
+        const productoDiv = document.createElement('div');
+        productoDiv.innerHTML = `<p>${producto.product} - $${producto.precio}</p>
+        <button onclick="addToCart(${producto.id},1)" class="botonGrey">Agregar al Carrito</button>`;
+        listaProducto.appendChild(productoDiv);
+    });
 };
+
+//Función para crear lista  del carrito
+function renderCart() {
+    const carritoDiv = document.getElementById('carrito');
+    carritoDiv.innerHTML = ''; //limpio
+    cart.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.innerHTML = `<p>ID: ${item.id} Nombre: ${item.product} Cantidad: ${item.cantidad} Precio total: $${item.precioTotal}</p>`;
+        carritoDiv.appendChild(itemDiv);
+    });
+    const cantidadItems = cart.reduce((acumulador, item) => acumulador + item.cantidad, 0)
+    const totalItems = cart.reduce((acumulador, item) => acumulador + item.precioTotal, 0)
+    const totalP = document.createElement('p');
+    totalP.className = 'totalCarrito';
+    totalP.innerHTML = `TOTAL: $${totalItems} (${cantidadItems} productos)`;
+    carritoDiv.appendChild(totalP);
+
+    const buttonLimpiar = document.createElement('button');
+    buttonLimpiar.className = 'vaciarCart';
+    buttonLimpiar.textContent='Vaciar carrito';
+    carritoDiv.appendChild(buttonLimpiar);
+    buttonLimpiar.addEventListener('click',limpiarCarrito);
+};
+
+
+
+//Función para alamacenar carrito en el LocalStorage (JSON)
+function guardarCartEnLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+};
+
+//Recupero carrito de LocalStorage
+function cargarCartdeLocalStorage() {
+    const carritoGuardado = localStorage.getItem('cart');
+    return carritoGuardado ? JSON.parse(carritoGuardado) : []; //Si carrito guardado = true, desglosa el JSON del carrito, si no cre aarray vacío.
+};
+
+//Función para limpiar carrito
+function limpiarCarrito() {
+    cart = [];
+    localStorage.clear;
+    
+    const carritoDiv = document.getElementById('carrito');
+    carritoDiv.innerHTML = 'No hay productos en el carrito'; //limpio
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderProductos();
+    renderCart();
+    limpiarCarrito();
+});
+
+
+
+
+
+
+
